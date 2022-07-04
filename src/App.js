22a135f902cf6a9b8e5515e9d777876a05a20dbb
spellhaven <j_jisu@protonmail.com>
2022-07-04 16:18:11 +0900

    React는 딱 그런 놈이다, 성격도 뭣도 이상하지만 친구가 너무 많아서 어쩔 수 없이 다들 좋아해야 하는 척 하는 친구...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+function InputTest() {
+
+  const [inputText, setInputText] = useState("Hello World!");
+
+  const onReset = () => {
+    setInputText("");
+  }
+
+  const onTextChange = (text) => {
+    setInputText(text.target.value); // text가 객체로 인식되니까, 드러운(들어온) 값을 그대로 쓰고 싶을 땐 .target.value를 붙여 보세요!
+  }
+
+  return (
+    <div>
+      <input onChange={onTextChange} value = {inputText}></input>
+      <button onClick={onReset}>촉이와(초기화)</button>
+      <div>
+        <h3>입력값: {inputText}</h3>
+      </div>
+    </div>
+  );
+}
+
+function Counter() {
+  
+  const [num, setNum] = useState(0);
+  // const [변수이름, 세터이름] = useState(초기값);
+
+  const onPlus = () => {
+    setNum(num + 1);
+  }
+  const onMinus = ()=> {
+    setNum(num - 1);
+  }
+  
+  // const onPlus = ()=> {
+  //   세터(새로운값);
+  // }
+  // const onMinus = ()=> {
+  //   세터(새로운값);
+  // }
+  
+  return (
+    <div>
+        <h1>{num}</h1>
+        {/* <h1>{변수이름}</h1> */}
+        <button onClick={onPlus}>+1</button>
+        <button onClick={onMinus}>-1</button>
+    </div>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Counter></Counter>
+        <hr></hr>
+        <InputTest></InputTest>
     </div>
   );
 }
